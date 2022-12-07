e80873d8ef7b75ec5e27c189c9c9a56809332ad5
Ash2803 <ashot.93@mail.ru>
2022-12-08 03:50:06 +0400

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -11,7 +11,9 @@ class AuthorAdmin(admin.ModelAdmin):
         'price',
         'new_building',
         'construction_year',
-        'town'
+        'town',
+        'owners_phonenumber',
+        'owner_pure_phone'
     ]
     list_editable = [
         'new_building'
