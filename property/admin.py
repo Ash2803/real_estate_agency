ae066bd7a7e4a094aadf3187d3f0761140c3b569
Ash2803 <ashot.93@mail.ru>
2022-12-12 01:29:23 +0400

    Deleted redundant fields from Flat model

--- property/admin.py
+++ property/admin.py
@@ -20,8 +20,6 @@ class FlatsAdmin(admin.ModelAdmin):
         'new_building',
         'construction_year',
         'town',
-        'owners_phonenumber',
-        'owner_pure_phone'
     ]
     list_editable = [
         'new_building'
