a6c5345498f118186d6bef81c02abd36c90cb50b
Ash2803 <ashot.93@mail.ru>
2022-12-14 01:39:32 +0400

    Fixed 0005 migration, deleted redundant code

--- property/migrations/0005_auto_20221206_0052.py
+++ property/migrations/0005_auto_20221206_0052.py
@@ -7,17 +7,8 @@ class Migration(migrations.Migration):
 
     def is_building_new(self, schema_editor):
         Flat = self.get_model('property', 'Flat')
-        flats = Flat.objects.all()
-        flats_iterator = flats.iterator()
-        try:
-            first_flat = next(flats_iterator)
-        except StopIteration:
-            pass
-        else:
-            from itertools import chain
-            for flat in chain([first_flat], flats_iterator):
-                flat.new_building = (flat.construction_year >= 2015)
-                flat.save()
+        Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+        Flat.objects.filter(construction_year__lt=2015).update(new_building=True)
 
     dependencies = [
         ('property', '0004_auto_20221205_1640'),
